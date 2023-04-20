a951bd65017c90b3c16746d6a0f8f18cf3b8b402
Connor Chevli <connorchev@gmail.com>
2023-04-20 10:44:38 -0400

    Initial Commit

---
+++ rollup.config.mjs
@@ -0,0 +1,30 @@
+import typescript from '@rollup/plugin-typescript';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        dir: 'dist',
+        format: 'cjs',
+      },
+    ],
+    plugins: [
+      // Compile TypeScript files
+      typescript({ tsconfig: './tsconfig.json', sourceMap: false }),
+      nodeResolve(),
+
+      // Copy plugin.json to dist
+      copy({
+        targets: [
+          {
+            src: 'plugin.json',
+            dest: 'dist',
+          },
+        ],
+      }),
+    ],
+  },
+];
